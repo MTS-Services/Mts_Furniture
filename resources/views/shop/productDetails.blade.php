ac327e74e823ee89bd6bd1582eb5bfa2e3c3f440
Rashed Miya <132655219+rashedsmiya@users.noreply.github.com>
2025-07-21 11:55:18 +0600

    add

---
+++ resources/views/shop/productDetails.blade.php
@@ -0,0 +1,17 @@
+@extends('layout.layout')
+
+@php
+    $title='Product';
+    $subTitle = 'Shop';
+    $subTitle2 = 'Product';
+    $script= '<script src="' . asset('assets/js/vendors/zoom.js') . '"></script>
+              <script src="' . asset('assets/partials/script.js') . '"></script>';
+@endphp
+
+@section('content')
+
+     <h1><br></h1>
+     <h1><br></h1>
+     <h1><br></h1>
+
+@endsection
